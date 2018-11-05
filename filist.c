fd81108e38f93c9925a302be37d51a842a23a58e
Jane Guleski <jguleski@e1z2r3p47.42.us.org>
2018-11-05 15:52:22 -0800

    izmeni vo copy node int ke ja napram

--- filist.c
+++ filist.c
@@ -6,7 +6,7 @@
 /*   By: jguleski <jguleski@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/03 23:41:07 by jguleski          #+#    #+#             */
-/*   Updated: 2018/11/04 23:43:57 by jguleski         ###   ########.fr       */
+/*   Updated: 2018/11/05 15:26:24 by jguleski         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,7 +35,7 @@ void	store_play(t_board *board, t_filist **main_list)
 	}
 	newplay->t_id = board->t_id;
 	if (newplay->t_id == (*main_list)->t_id)
-		put_down(main_list, newplay);
+		put_down(*main_list, newplay);
 	else
 	{
 		newplay->next = *main_list;
@@ -57,30 +57,42 @@ void	put_down(t_filist *main_list, t_filist *new)
 	new->down = NULL;
 }
 
-void	find_solution(t_board *board, t_filist *playlist)
+/*
+** Gets the first valid play from figure A, then goes to the next figure, checks with B0
+** if it's not valid, it goes down, checks with B1, if it's valid, stores it and goes to next
+** C0, if good adds it to list, if no C element is good it deletes the stored elements from the list.
+** If C0 was good it is added and has a complete solution {A0;B1;C0} then it adds a separator in the list,
+** and continues from B2, then org will be init. And it will compare B2 with figure before it in the
+** completed solution. if B2 is good with A0, it copies A0, then B2 is added, and contines to C ...
+*/
+
+void	find_solution(t_board *board, t_filist *playlist, t_filist	*org)
 {
 	static t_filist	*solutions = NULL;
 	t_filist	*temp;
-	int	count;
 
 	temp = playlist;
-	count = 0;
 	while (temp)
 	{
+		if (org)
+			compare_prev_solution(&org, temp);
 		if (compare_cords (solutions, temp) == 0)
 		{
-			copy_node(solutions, temp, board->pieces);
-			find_solution(board, playlist->next);
-			count++;
+			org = copy_node(&solutions, temp, board->pieces);
+			find_solution(board, playlist->next, org);
 		}
+	// if (temp->t_id == 'A')
+	// 	org = NULL;
 		temp = temp->down;
 	}
-	return (count)
+	clean_garbage()
 }
 
-		//copy_node(solution, temp->next);
-		// if (x == board->pieces)
-		// 	break;
+void	clean_garbage(t_filist	*solution)
+{
+
+
+}
 
 int	compare_cords(t_filist	*solution, t_filist	*new_elem)
 {
@@ -108,8 +120,7 @@ int	compare_cords(t_filist	*solution, t_filist	*new_elem)
 	return (0);
 }
 
-
-void	copy_node(t_filist **dest, t_filist *source, int pieces)
+t_filist	*copy_node(t_filist **dest, t_filist *source, int pieces)
 {
 	int x;
 	int y;
@@ -135,19 +146,37 @@ void	copy_node(t_filist **dest, t_filist *source, int pieces)
 		new_node = new_node->next;
 		x++;
 	}
-	if (x == pieces)
-		add_separator (*dest);
+	return (x == pieces ? add_separator(dest) : NULL);
 	//score mozda ne trebit ovde da se kopirat, na kraj samo da se sum koordinatite
 }
 
-void	add_separator(t_filist **solutions)
+t_filist	*add_separator(t_filist **solutions)
 {
 	t_filist *new_node;
-	if (!solutions || (new_node = malloc(sizeof(t_filist))) == NULL)
+	if (((new_node = malloc(sizeof(t_filist))) == NULL) || !(*solutions))
 		exit_app("Malloc failed inside copy_node");
-	new_node->t_id == '$';
-	new_node->down == NULL;
+	new_node->t_id = '$';
+	new_node->down = NULL;
 	new_node->next = *solutions;
-	solutions = new_node;
+	*solutions = new_node;
+	return (*solutions);
 }
 
+void	compare_prev_solution(t_filist **org, t_filist *new_elem)
+{
+	t_filist *temp;
+
+	temp = *org;
+	if (temp->t_id != '$')
+		exit_app("Wrongly assigned original");
+	
+	while (temp && temp->t_id != new_elem->t_id)
+		temp = temp->next;
+	temp = temp->next;
+	if (compare_cords(temp, new_elem) == 0)
+		while (temp && temp->t_id != '$')
+		{
+			copy_node(org, temp, -1);
+			temp = temp->next;
+		}
+}
